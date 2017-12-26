8cfaa35604c8f7daa052482c3563d9aa5844828f
Vaibhav Gharad <vaibhav.gharad@pb.com>
2017-12-26 16:35:20 +0530

    changes for lombok jar
    Changes for lombok

--- src/main/java/com/reviews/app/models/OutputTable.java
+++ src/main/java/com/reviews/app/models/OutputTable.java
@@ -2,9 +2,9 @@ package com.reviews.app.models;
 
 import java.util.Date;
 
-import org.springframework.data.mongodb.core.mapping.Document;
-
 import lombok.Data;
+
+import org.springframework.data.mongodb.core.mapping.Document;
 
 @Data
 @Document(collection = "output_table")
