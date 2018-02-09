f3e56795bd9e817352c5b2126ad3814c873d99fb
Tarun Singh <tarun.singh@pb.com>
2018-02-09 15:50:23 +0530

    Correcting date output

--- src/main/java/com/reviews/app/models/OutputTable.java
+++ src/main/java/com/reviews/app/models/OutputTable.java
@@ -1,11 +1,15 @@
 package com.reviews.app.models;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AccessLevel;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Document(collection = "output_table")
@@ -28,10 +32,17 @@ public class OutputTable {
 	private String l3;
 	private String l0;
 	private String l1;
+	@Getter(AccessLevel.NONE)
 	private Date date;
 	private String unique_id;
 	private String exec_Status;
 	private String feedback;
 	private boolean correctCategory;
+
+	public String getDate() {
+		DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+		String d = df.format(date);
+		return d;
+	}
 
 }
